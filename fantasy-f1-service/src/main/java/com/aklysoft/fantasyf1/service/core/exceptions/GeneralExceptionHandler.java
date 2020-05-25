a2eb3cfe8e6cb63e14bea551ee1c656ae63f8141
Szilárd Antal <sziszo@gmail.com>
2020-05-25 22:06:38 +0200

    Configure the generic exception handler for fantasy-f1-service #7 (#9)

---
+++ fantasy-f1-service/src/main/java/com/aklysoft/fantasyf1/service/core/exceptions/GeneralExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.aklysoft.fantasyf1.service.core.exceptions;
+
+import com.aklysoft.fantasyf1.service.core.AppConfiguration;
+import org.codehaus.plexus.util.ExceptionUtils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@ApplicationScoped
+public class GeneralExceptionHandler implements ExceptionMapper<Exception> {
+
+  private final AppConfiguration appConfiguration;
+
+  public GeneralExceptionHandler(AppConfiguration appConfiguration) {
+    this.appConfiguration = appConfiguration;
+  }
+
+  @Override
+  public Response toResponse(Exception exception) {
+    StringBuilder rsp = new StringBuilder("An error occurred!");
+    if (exception.getMessage() != null) {
+      rsp.append("message: ").append(exception.getMessage());
+    }
+
+    if ("true".equals(appConfiguration.showStacktrace)) {
+      rsp.append("\n").append(ExceptionUtils.getStackTrace(exception));
+    }
+
+    return Response
+            .status(Response.Status.INTERNAL_SERVER_ERROR)
+            .entity(rsp.toString())
+            .build();
+  }
+
+}
