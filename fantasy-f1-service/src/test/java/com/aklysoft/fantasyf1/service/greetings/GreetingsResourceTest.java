e1528f0b38e291e3d78a1e61a14202af4fb7e8fa
Szilárd Antal <sziszo@gmail.com>
2020-05-25 22:01:37 +0200

    Configure ORM for fantasy-f1-service #6 (#8)

--- fantasy-f1-service/src/test/java/com/aklysoft/fantasyf1/service/greetings/GreetingsResourceTest.java
+++ fantasy-f1-service/src/test/java/com/aklysoft/fantasyf1/service/greetings/GreetingsResourceTest.java
@@ -1,21 +1,22 @@
-package com.aklysoft;
+package com.aklysoft.fantasyf1.service.greetings;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class GreetingsResourceTest {
 
   @Test
   public void testHelloEndpoint() {
     given()
-            .when().get("/hello")
+            .when().get("/api/v1/hello")
             .then()
             .statusCode(200)
-            .body(is("hello"));
+            .body(containsString("hello"));
   }
 
 }
---
+++ fantasy-f1-service/src/test/java/com/aklysoft/fantasyf1/service/greetings/NativeGreetingsResourceIT.java
@@ -0,0 +1,9 @@
+package com.aklysoft.fantasyf1.service.greetings;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingsResourceIT extends GreetingsResourceTest {
+
+  // Execute the same tests but in native mode.
+}
